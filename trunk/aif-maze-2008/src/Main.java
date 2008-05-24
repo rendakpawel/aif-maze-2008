cd1862c27c9389e1f2489c97ceb4450bd40c9deb
lukasz.b <lukasz.b@7f762061-fc4d-0410-b6bb-7fc964809690>
2008-05-24 17:02:26 +0000

    Bzdurny commit :)

--- trunk/aif-maze-2008/src/Main.java
+++ trunk/aif-maze-2008/src/Main.java
@@ -2,10 +2,14 @@
 public class Main {
 
 	/**
-	 * @param args
+	 * The greatest maze project ever!
+	 * 
+	 * @author £ukasz Barcikowski
+	 * @author Pawe³ Rendak
+	 * @param args Command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
