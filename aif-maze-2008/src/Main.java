73183cac77920acd452eae2c9d37afc1559e045a
rendak.pawel <rendak.pawel@7f762061-fc4d-0410-b6bb-7fc964809690>
2008-05-26 15:32:18 +0000

    block

--- aif-maze-2008/src/Main.java
+++ aif-maze-2008/src/Main.java
@@ -1,3 +1,6 @@
+import board.Board;
+
+
 
 public class Main {
 
@@ -9,6 +12,16 @@ public class Main {
 	 * @param args Command line arguments
 	 */
 	public static void main(String[] args) {
+		
+		tests();
+	}
+	
+	
+	
+	static private void tests(){
+		
+		Board b = new Board(8,4);
+		b.writeToConsoleBlocks();
 		
 	}
 
