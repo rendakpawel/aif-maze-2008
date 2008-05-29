71ba1b5607ca46a0f15d1859eb355b9cf13ece4e
lukasz.b <lukasz.b@7f762061-fc4d-0410-b6bb-7fc964809690>
2008-05-29 13:20:46 +0000

    Some debug mode changes. Solving does not work properly... It finds solution, but it doesn't turn always the same way...

--- aif-maze-2008/src/Main.java
+++ aif-maze-2008/src/Main.java
@@ -1,3 +1,9 @@
+import java.awt.Point;
+import java.util.Stack;
+
+import exceptions.CellsAreNotNeighborsException;
+import exceptions.OutOfBoardException;
+
 import maze.Maze;
 
 
@@ -12,8 +18,23 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		Maze maze = new Maze(20,20);
-		System.out.println("\n\nSOLVING MAZE...\n\n");
+		Maze maze = new Maze(9,9);
+		maze.setDebugMode(false);
+		
+		try {
+			maze.generateMaze();
+		} catch (CellsAreNotNeighborsException e) {
+			System.err.println("[Algorithm error] Generation tried to operate on non-neighboring cells!");
+		} catch (OutOfBoardException e) {
+			System.err.println("[Algorithm error] Generation went out of board range!");
+			}
+		maze.printMaze();
+		
 		maze.solveMaze();
+		Stack<Point> mazeSolution = maze.getSolution();
+		System.out.println("\nSolution (" + mazeSolution.size() + " steps):\n");
+		for (Point point : mazeSolution) {
+			System.out.println("Go to [" + point.x + "," + point.y + "]");
+		}
 	}
 }
