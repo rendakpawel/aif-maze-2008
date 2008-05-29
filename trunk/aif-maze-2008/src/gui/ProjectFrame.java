7291ee16b964623dea6e95fbfd1c353fcd40140d
rendak.pawel <rendak.pawel@7f762061-fc4d-0410-b6bb-7fc964809690>
2008-05-29 20:00:42 +0000

    ProjectFrame 1st try.

---
+++ trunk/aif-maze-2008/src/gui/ProjectFrame.java
@@ -0,0 +1,102 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JSplitPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+
+
+public class ProjectFrame {
+
+
+	private JFrame jFrame = null;  //  @jve:decl-index=0:visual-constraint="30,5"
+	private JPanel jContentPane = null;
+	private JSplitPane jSplitPane = null;
+	private JMenuBar jJMenuBar = null;
+	private JMenu jMenu = null;
+
+	/**
+	 * This method initializes jFrame	
+	 * 	
+	 * @return javax.swing.JFrame	
+	 */
+	private JFrame getJFrame() {
+		if (jFrame == null) {
+			jFrame = new JFrame();
+			jFrame.setSize(new Dimension(498, 359));
+			jFrame.setJMenuBar(getJJMenuBar());
+			jFrame.setContentPane(getJContentPane());
+		}
+		return jFrame;
+	}
+
+	/**
+	 * This method initializes jContentPane	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getJContentPane() {
+		if (jContentPane == null) {
+			jContentPane = new JPanel();
+			jContentPane.setLayout(new BorderLayout());
+			jContentPane.add(getJSplitPane(), BorderLayout.CENTER);
+		}
+		return jContentPane;
+	}
+
+	/**
+	 * This method initializes jSplitPane	
+	 * 	
+	 * @return javax.swing.JSplitPane	
+	 */
+	private JSplitPane getJSplitPane() {
+		if (jSplitPane == null) {
+			jSplitPane = new JSplitPane();
+			jSplitPane.setDividerLocation(400);
+		}
+		return jSplitPane;
+	}
+
+	/**
+	 * This method initializes jJMenuBar	
+	 * 	
+	 * @return javax.swing.JMenuBar	
+	 */
+	private JMenuBar getJJMenuBar() {
+		if (jJMenuBar == null) {
+			jJMenuBar = new JMenuBar();
+			jJMenuBar.setPreferredSize(new Dimension(0, 26));
+			jJMenuBar.add(getJMenu());
+		}
+		return jJMenuBar;
+	}
+
+	/**
+	 * This method initializes jMenu	
+	 * 	
+	 * @return javax.swing.JMenu	
+	 */
+	private JMenu getJMenu() {
+		if (jMenu == null) {
+			jMenu = new JMenu();
+			jMenu.setText("Close");
+			jMenu.setMnemonic(KeyEvent.VK_C);
+		}
+		return jMenu;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asd");
+		
+	}
+
+}
